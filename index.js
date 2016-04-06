1c928f6629d6c67785a9fdd8d2339cfae5213c43
楼教主 <cik520@qq.com>
2016-04-06 20:59:35 +0800

    调整结构

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ hexo.extend.generator.register(function(site) { // 注册插件
     var posts = []; // 所有文章
     var tags = {}; // 所有标签
 
-    site.posts.sort('-date').forEach(function(page) { // 按日期排序后遍历文章
+    site.posts.sort('-date').forEach(function(page, idx) { // 按日期排序后遍历文章
         posts.push({
             title: page.title, // 标题
             date: cfg.date ? page.date.format('YYYY-MM-DD HH:mm:ss') : undefined, // 日期
@@ -12,13 +12,13 @@ hexo.extend.generator.register(function(site) { // 注册插件
         });
 
         page.tags && page.tags.forEach(function(tag) { // 标签
-            tags[tag.name] = 1;
+            (tags[tag.name] || (tags[tag.name] = [])).push(idx);
         });
     });
 
     var data = JSON.stringify({
         posts: posts,
-        tags: Object.keys(tags)
+        tags: tags
     });
 
     if (cfg.is_jsonp) { // jsonp 模式
